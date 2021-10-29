77c6272ede9bbce4e6da3386b0e7721d0f3de371
Jack McNary <jack.mcnary@gmail.com>
2021-10-29 13:28:22 -0600

    adding readme criteria

---
+++ Assets/JS/index.js
@@ -0,0 +1,53 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const createMarkdown = require("Assets/JS/createMarkdown.js");
+
+const readmeQuestions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: License,
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: Contributing,
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: Tests,
+        message: "Give your readme a Title!",
+    },
+    {
+        type: 'input',
+        name: Questions,
+        message: "Give your readme a Title!",
+    }
+
+
+
+
+
+
+
+]
